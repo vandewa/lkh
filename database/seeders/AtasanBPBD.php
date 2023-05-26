d18502721343133a327d995eef648e2a97a607bc
Devan Dewananta <devandewananta@gmail.com>
2023-05-26 10:19:55 +0700

    tambah seeder atasan bpbd

--- database/seeders/AtasanBPBD.php
+++ database/seeders/AtasanBPBD.php
@@ -18,7 +18,10 @@ class AtasanBPBD extends Seeder
             ['id' => '3', 'nama' => 'Drs. GANESWARA WIBAWA, M.M.', 'nip' => '196801041992031008', 'jabatan' => 'Kepala Bidang Kedaruratan dan Logistik', 'opd_tp' => 'OPD_TP_01'],
             ['id' => '4', 'nama' => 'MUSAFAK, S.IP.', 'nip' => '196706141989121002', 'jabatan' => 'Kepala Bidang Pemadam Kebakaran dan Penyelamatan', 'opd_tp' => 'OPD_TP_01'],
             ['id' => '5', 'nama' => 'ROBINGAH, S.IP., M.M.', 'nip' => '197002111996032005', 'jabatan' => 'Kepala Bidang Pencegahan, Kesiapsiagaan, Rehabilitasi dan Rekonstruksi', 'opd_tp' => 'OPD_TP_01'],
-        ]
+        ];
         
+        foreach ($data as $datum) {
+            Atasan::create($datum);
+        }
     }
 }
