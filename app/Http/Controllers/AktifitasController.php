6334d76dc945ab853e19ce85867ceda7c196c55c
Devan Dewananta <devandewananta@gmail.com>
2023-10-16 09:16:19 +0700

    tambah limit

--- app/Http/Controllers/AktifitasController.php
+++ app/Http/Controllers/AktifitasController.php
@@ -30,13 +30,13 @@ class AktifitasController extends Controller
             $data = Aktifitas::with(['nama_usernya'])->where('keterangan', null);
 
             if (auth()->user()->hasRole('superadmin')) {
-                $data;
+                $data->limit(1000);
             } elseif (auth()->user()->hasRole(['admin-dpupr', 'admin-bpbd'])) {
                 $data->whereHas('nama_usernya', function ($a) {
                     $a->where('opd_tp', auth()->user()->opd_tp);
-                });
+                })->limit(1000);
             } else {
-                $data->where('user_id', auth()->user()->id);
+                $data->where('user_id', auth()->user()->id)->limit(1000);
             }
 
             $data = $data->select('*');
